315e323f23f03699a0c05a236c10ea7b581cc728
matthewmillerh <matthewmillerh@outlook.com>
2025-04-16 16:32:02 +0200

    made a component for project card display, added redirects file to fix router views loading on netlify

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import TechStack from '@/components/TechStack.vue'
+
+defineProps({
+  imageName: String,
+  repoName: String,
+  liveLink: String,
+})
+</script>
+<template>
+  <div
+    class="w-full md:w-4/5 min-h-[650px] p-6 flex justify-center flex-col items-center rounded-md shadow-md bg-gradient-to-br from-gray-100 to-gray-50 dark:bg-gradient-to-br dark:from-gray-700/75 dark:to-gray-800/75 transition-colors duration-300 ease-in-out"
+  >
+    <a :href="liveLink" target="_blank" class="text-center">
+      <h1 class="font-semibold">
+        <span><slot name="project-title"></slot></span>
+        <span class="ml-2">
+          <font-awesome-icon icon="fa-arrow-up-right-from-square" size="sm" />
+        </span>
+      </h1>
+      <p class="italic text-sm"><slot name="date"></slot></p>
+      <img :src="'/img/' + imageName" alt="" class="mt-2 max-h-96 rounded-md shadow-md" />
+    </a>
+    <!-- <p class="mt-4 py-0.5 w-full md:w-4/5 text-justify border-b-2 border-t-2 border-gray-400"> -->
+    <p
+      class="mt-4 py-1 px-2 w-full md:w-4/5 text-justify bg-gray-200/70 dark:bg-gray-700/70 rounded-lg shadow-md"
+    >
+      <slot name="description"></slot>
+    </p>
+    <TechStack :repo-prop="repoName" />
+
+    <!-- Button links to the project -->
+    <div class="mt-4 flex justify-center gap-2">
+      <button class="inline-flex items-center px-4 py-2 bg-slate-600 rounded-md shadow-md">
+        <span class="mr-2 font-semibold">Visit Live Site</span>
+        <span><font-awesome-icon icon="fa-arrow-up-right-from-square" size="sm" /></span>
+      </button>
+      <button class="inline-flex items-center px-4 py-2 bg-slate-600 rounded-md shadow-md">
+        <span class="mr-2 font-semibold">View Github Repo</span>
+        <span><font-awesome-icon icon="fa-brands fa-github" size="xl" /></span>
+      </button>
+    </div>
+  </div>
+</template>
--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,105 +1,64 @@
 <script setup>
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import TechStack from '@/components/TechStack.vue'
+import ProjectCard from '@/components/ProjectCard.vue'
 </script>
 <template>
   <main>
     <div class="w-full flex flex-col items-center gap-8">
       <!-- Artist portfolio website -->
-      <div
-        class="w-full md:w-4/5 min-h-[650px] p-6 flex justify-center flex-col items-center rounded-md shadow-md bg-gradient-to-br from-gray-100 to-gray-50 dark:bg-gradient-to-br dark:from-gray-700/75 dark:to-gray-800/75 transition-colors duration-300 ease-in-out"
-      >
-        <a href="https://www.pentique.co.za" target="_blank" class="text-center">
-          <h1 class="font-semibold">
-            Artist Portfolio Website
-            <span class="ml-2">
-              <font-awesome-icon icon="fa-arrow-up-right-from-square" size="sm" />
-            </span>
-          </h1>
-          <p class="italic text-sm">2024 - present</p>
-          <img
-            :src="'/img/project_rae_smith.png'"
-            alt=""
-            class="mt-2 max-h-96 rounded-md shadow-md"
-          />
-        </a>
-        <p class="mt-4 w-full md:w-4/5 text-justify">
+      <ProjectCard image-name="project_rae_smith.png" repo-name="raesmith" live-link="">
+        <template #project-title>Artist Portfolio Website</template>
+        <template #date>2024 - present</template>
+        <template #description>
           This project is an ongoing effort to create a dedicated online portfolio for South African
           artist Emily Rae Smith Labuschagne. The final website will provide an efficient and
           professional platform to showcase her artistic works.
-        </p>
-        <TechStack repo-prop="raesmith" />
-      </div>
+        </template>
+      </ProjectCard>
 
       <!-- Online Store for small business -->
-      <div
-        class="w-full md:w-4/5 min-h-[650px] p-6 flex justify-center flex-col items-center rounded-md shadow-md bg-gradient-to-br from-gray-100 to-gray-50 dark:bg-gradient-to-br dark:from-gray-700/75 dark:to-gray-800/75 transition-colors duration-300 ease-in-out"
+      <ProjectCard
+        image-name="project_pentique.png"
+        repo-name="pentique"
+        live-link="http://www.pentique.co.za"
       >
-        <a href="https://www.pentique.co.za" target="_blank" class="text-center">
-          <h1 class="font-semibold">
-            Online Store for Small Business
-            <span class="ml-2">
-              <font-awesome-icon icon="fa-arrow-up-right-from-square" size="sm" />
-            </span>
-          </h1>
-          <p class="italic text-sm">2024 - present</p>
-          <img :src="'/img/project_pentique.png'" alt="" class="mt-2 max-h-96 rounded-md" />
-        </a>
-        <p class="mt-4 w-full md:w-4/5 text-justify">
+        <template #project-title>Online Store for Small Business</template>
+        <template #date>2024 - present</template>
+        <template #description>
           Originally developed in PHP with a custom content management system, this website is
           undergoing a complete rewrite using a modern tech stack. The project is transitioning to a
           Vue.js frontend and an Express.js backend to enhance performance, security, and
           maintainability, addressing previous vulnerabilities identified in the initial
           implementation.
-        </p>
-        <TechStack repo-prop="pentique" />
-      </div>
+        </template>
+      </ProjectCard>
 
       <!-- Travel Itinerary Website -->
-      <div
-        class="w-full md:w-4/5 min-h-[650px] p-6 flex justify-center flex-col items-center rounded-md shadow-md bg-gradient-to-br from-gray-100 to-gray-50 dark:bg-gradient-to-br dark:from-gray-700/75 dark:to-gray-800/75 transition-colors duration-300 ease-in-out"
+      <ProjectCard
+        image-name="project_itinerary.png"
+        repo-name="itinerary"
+        live-link="https://matthewmillerh-itinerary.netlify.app/"
       >
-        <a href="https://matthewmillerh-itinerary.netlify.app/" target="_blank" class="text-center">
-          <h1 class="font-semibold">
-            Travel Itinerary Website
-            <span class="ml-2">
-              <font-awesome-icon icon="fa-arrow-up-right-from-square" size="sm" />
-            </span>
-          </h1>
-          <p class="italic text-sm">2024</p>
-          <img
-            :src="'/img/project_itinerary.png'"
-            alt=""
-            class="mt-2 max-h-96 rounded-md shadow-md"
-        /></a>
-        <p class="mt-4 w-full md:w-4/5 text-justify">Description</p>
-        <TechStack repo-prop="itinerary" />
-      </div>
+        <template #project-title>Travel Itinerary</template>
+        <template #date>2024</template>
+        <template #description>
+          Created before a lengthy trip to Europe, this web application started as a way to produce
+          a presentable itinerary for a Schengen Visa. Driven by personal interest, additional
+          functionalities were integrated, expanding its capabilities beyond the initial
+          requirement.
+        </template>
+      </ProjectCard>
 
       <!-- The Photographers Gallery ZA -->
-      <div
-        class="w-full md:w-4/5 min-h-[650px] p-6 flex justify-center flex-col items-center rounded-md shadow-md bg-gradient-to-br from-gray-100 to-gray-50 dark:bg-gradient-to-br dark:from-gray-700/75 dark:to-gray-800/75 transition-colors duration-300 ease-in-out"
+      <ProjectCard
+        image-name="project_photographersgalleryza.png"
+        repo-name="thephotographersgallery"
+        live-link=""
       >
-        <a
-          href="https://github.com/matthewmillerh/thephotographersgallery"
-          target="_blank"
-          class="text-center"
-        >
-          <h1 class="font-semibold">
-            Photo Gallery Website for Cape Town Art Gallery
-            <span class="ml-2">
-              <font-awesome-icon icon="fa-arrow-up-right-from-square" size="sm" />
-            </span>
-          </h1>
-          <p class="italic text-sm">2015 (offline)</p>
-          <img
-            :src="'/img/project_photographersgalleryza.png'"
-            alt=""
-            class="mt-2 max-h-96 rounded-md shadow-md"
-        /></a>
-        <p class="mt-4 w-full md:w-4/5 text-justify">Description</p>
-        <TechStack repo-prop="thephotographersgallery" />
-      </div>
+        <template #project-title>Photo Gallery Website for Cape Town Art Gallery</template>
+        <template #date>2015 (offline)</template>
+        <template #description>Test</template>
+      </ProjectCard>
     </div>
   </main>
 </template>
